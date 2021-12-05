1868e7311992f3fddf3996b660b217d08b9988d0
aniskhonakhmedov <nipanis2007@gmail.com>
2021-12-05 21:51:19 +0500

    fuckkkkkkkkkk

---
+++ js/app.js
@@ -0,0 +1,231 @@
+import music from './data.js'
+if (!localStorage.liked) localStorage.liked = []
+
+let data = music
+let leftRandom = document.querySelector('.left-random')
+let midLiked = document.querySelector('.mid-liked')
+let RightListened = document.querySelector('.right-listened')
+let activeButton = document.querySelector(".menu.active");
+
+let liked_for_local = localStorage.liked.split('') || []
+let forAddFavorite = []
+
+let random_start = 1; // От какого генерировать
+let random_end = data.length; // До какого генерировать
+
+let allСycles = data.length;
+
+let array = []
+
+for (let i = random_start; i <= random_end; i++) {
+    array.push(i)
+}
+let numbers = []
+
+for (let countCycles = 1; countCycles <= allСycles; countCycles++) {
+    numbers.push(array.splice(Math.random() * array.length, 1)[0])
+}
+numbers.splice(11)
+
+let p_rand = document.createElement('p')
+p_rand.innerHTML = '10 random songs'
+p_rand.classList.add('item')
+
+let p_liked = document.createElement('p')
+p_liked.innerHTML = 'liked tracks'
+p_liked.classList.add('item')
+
+let num_num = 0
+
+const reload = (track) => {
+    leftRandom.innerHTML = ''
+    leftRandom.append(p_rand)
+
+    midLiked.innerHTML = ''
+    midLiked.append(p_liked)
+    num_num = 0
+    for (let item of track) {
+        for (let num of numbers) {
+            if (item.id == num) {
+                let div_main = document.createElement('div')
+                let div_num = document.createElement('div')
+                let div_info = document.createElement('div')
+                let div_img = document.createElement('div')
+                let div_about = document.createElement('div')
+                let div_name = document.createElement('div')
+                let div_time_menu = document.createElement('div')
+                let div_menu = document.createElement('div')
+                let img_author = document.createElement('img')
+                let img_circle = document.createElement('span')
+                let p_name = document.createElement('p')
+                let p_author = document.createElement('p')
+                let p_time = document.createElement('p')
+                let p_like = document.createElement('p')
+                let p_add = document.createElement('p')
+                let p_listen = document.createElement('p')
+
+
+                div_main.classList.add("track")
+                div_num.classList.add('num')
+                div_img.classList.add('img')
+                div_info.classList.add('info')
+                div_about.classList.add('about')
+                div_name.classList.add('name')
+                div_time_menu.classList.add('time-menu')
+                p_listen.classList.add("listen")
+                p_listen.classList.add("toUpper")
+                p_like.classList.add("toUpper")
+                p_like.classList.add('like')
+                p_add.classList.add("toUpper")
+                p_add.classList.add('add')
+                p_time.classList.add('time')
+                div_menu.classList.add("menu")
+                img_circle.classList.add('three-circle')
+
+
+                div_num.innerHTML = ++num_num
+                img_author.setAttribute('src', `./picture/${item.img}.jpg`)
+                img_author.style.width = '40px'
+                img_author.style.height = '40px'
+                p_name.innerHTML = item.title
+                p_author.innerHTML = item.author
+                img_circle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>`
+
+                p_like.innerHTML = "Like"
+                p_add.innerHTML = 'Add to playlist'
+                p_listen.innerHTML = 'Listen now'
+
+                p_like.onclick = () => like(item.id)
+
+                img_circle.onclick = () => {
+                    if (div_menu.classList.contains("active")) return;
+
+                    div_menu.classList.add("active");
+
+                    if (div_menu.classList.add("active")) {
+                        div_menu.onclick = () => div_menu.classList.remove("active");
+                    }
+                    if (activeButton) {
+                        activeButton.classList.remove("active");
+                    }
+                    activeButton = div_menu;
+                }
+
+                div_main.append(div_num, div_info, div_img, div_about)
+                div_img.append(img_author)
+                div_about.append(div_name, div_time_menu)
+                div_name.append(p_name, p_author)
+                div_time_menu.append(p_time, img_circle, div_menu)
+                div_menu.append(p_like, p_add, p_listen)
+                leftRandom.append(div_main)
+            }
+
+        } 
+        if (item.isLiked == true) {
+            let div_main = document.createElement('div')
+            let div_num = document.createElement('div')
+            let div_info = document.createElement('div')
+            let div_img = document.createElement('div')
+            let div_about = document.createElement('div')
+            let div_name = document.createElement('div')
+            let div_time_menu = document.createElement('div')
+            let div_menu = document.createElement('div')
+            let img_author = document.createElement('img')
+            let img_circle = document.createElement('span')
+            let p_name = document.createElement('p')
+            let p_author = document.createElement('p')
+            let p_time = document.createElement('p')
+            let p_like = document.createElement('p')
+            let p_add = document.createElement('p')
+            let p_listen = document.createElement('p')
+
+
+            div_main.classList.add("track")
+            div_num.classList.add('num')
+            div_img.classList.add('img')
+            div_info.classList.add('info')
+            div_about.classList.add('about')
+            div_name.classList.add('name')
+            div_time_menu.classList.add('time-menu')
+            p_listen.classList.add("listen")
+            p_listen.classList.add("toUpper")
+            p_like.classList.add("toUpper")
+            p_like.classList.add('like')
+            p_add.classList.add("toUpper")
+            p_add.classList.add('add')
+            p_time.classList.add('time')
+            div_menu.classList.add("menu")
+            img_circle.classList.add('three-circle')
+
+
+            img_author.setAttribute('src', `./picture/${item.img}.jpg`)
+            img_author.style.width = '40px'
+            img_author.style.height = '40px'
+            p_name.innerHTML = item.title
+            p_author.innerHTML = item.author
+            img_circle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>`
+
+            p_like.innerHTML = "Like"
+            p_add.innerHTML = 'Add to playlist'
+            p_listen.innerHTML = 'Listen now'
+
+            p_like.onclick = () => like(item.id)
+
+            img_circle.onclick = () => {
+                if (div_menu.classList.contains("active")) return;
+
+                div_menu.classList.add("active");
+
+                if (div_menu.classList.add("active")) {
+                    div_menu.onclick = () => div_menu.classList.remove("active");
+                }
+                if (activeButton) {
+                    activeButton.classList.remove("active");
+                }
+                activeButton = div_menu;
+            }
+
+            div_main.append(div_num, div_info, div_img, div_about)
+            div_img.append(img_author)
+            div_about.append(div_name, div_time_menu)
+            div_name.append(p_name, p_author)
+            div_time_menu.append(p_time, img_circle, div_menu)
+            div_menu.append(p_like, p_add, p_listen)
+            midLiked.append(div_main)
+        }
+        else{
+            
+        }
+    }
+}
+
+reload(data)
+
+
+const like = (details) => {
+    liked_for_local.push(details)
+    localStorage.liked = liked_for_local
+
+    let find = data.filter(item => item.id == details)[0]
+    find.isLiked = !find.isLiked
+
+    if (forAddFavorite.length == 0) {
+        reload(data)
+    } else reload(forAddFavorite) 
+}
+
+
+let p_name_now = document.querySelector('p[data-name="name"]')
+
+console.log(p_name_now);
+
+// const reload_name_autor_now = (param) =>{
+    
+// }
+
+// reload_name_autor_now(data)
+
+let listen_now = (param) => {
+    console.log(123);
+    console.log(param);
+}
---
+++ js/data.js
@@ -0,0 +1,147 @@
+let playlists = [{
+    title: 'liked',
+    music: []
+}]
+
+let music = [
+    // {
+    //     title: String,
+    //     author: String,
+    //     title_org: String,        
+    //     length: Number,
+    //     isLiked: false,
+    // },
+    {
+        title: 'lovely',
+        title_org: "lovely", //for audio in js
+        author: 'Khalid, Billie Eilish',
+        img: 'lovely',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'bad guy',
+        title_org: 'bad-guy',
+        author: 'BillieEilish',
+        img: 'badGuy',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Six Feet Under',
+        author: 'Billie Eilish',
+        title_org: "SixFeetUnder",
+        img: 'six',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Сан Ларан',
+        author: 'Платина, дора',
+        title_org: 'СанЛаран',
+        img: 'sanLoran',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Младшая сестра',
+        author: 'дора',
+        title_org: 'МладшаяСестра',
+        img: 'doradura',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Втюрилась',
+        author: 'дора',
+        title_org: 'Втюрилась',
+        img: 'vturilas',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Дорадура',
+        author: 'дора',
+        title_org: 'Дорадура',
+        img: 'doradura',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Toxic',
+        author: 'BoyWithUke',
+        title_org: 'Toxic',
+        img: 'toxic',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'MONEY',
+        author: 'LISA',
+        title_org: 'MONEY',
+        img: 'money',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'I WANNA BE YOUR SLAVE',
+        author: 'Måneskin',
+        title_org: 'IWANNABEYOURSLAVE',
+        img: 'wanna',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'PUSSY BOY',
+        author: 'ЕГОР КРИД',
+        title_org: 'PUSSYBOY',
+        img: 'pussy',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'LIPSI HA',
+        author: 'INSTASAMKA',
+        title_org: 'LIPSIHA',
+        img: 'lipsi',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'STAY (with Justin Bieber)',
+        author: 'The Kid LAROI, Justin Bieber',
+        title_org: 'stay',
+        img: 'stay',
+
+        length: Number,
+        isLiked: false,
+    },
+    {
+        title: 'Astronaut In The Ocean',
+        author: "Masked Wolf",
+        title_org: 'Astronaut',
+        img: 'ocean',
+
+        length: Number,
+        isLiked: false,
+    },
+]
+
+
+music.forEach((item, i) => {
+    item.id = i;
+});
+
+export default music
